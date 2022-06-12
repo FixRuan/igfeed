46febeab5aae7328febec5332cb1e50bfa6936d0
Ruan Pablo <ruan_goio10@hotmail.com>
2022-06-12 12:43:14 -0300

    Side bar component Style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,24 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
 
 export function SideBar() {
 
     return (
         <aside className={styles.sidebar}>
-            <img src="https://images.unsplash.com/photo-1573638732620-ab47b3fcfbef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" />
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1573638732620-ab47b3fcfbef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/FixRuan.png" alt="" />
+
                 <strong>Ruan Pablo</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#"></a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu Perfil
+                </a>
             </footer>
         </aside>
     )
