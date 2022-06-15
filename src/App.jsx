92d9e83fbcd5556f130c122e2d5a4481b2a0f5e9
Ruan Pablo <ruan_goio10@hotmail.com>
2022-06-15 19:01:59 -0300

    Post array format

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,49 @@ import styles from './App.module.css';
 import { Post } from "./components/Post";
 import { SideBar } from "./components/Sidebar";
 
+const posts = [
+    {
+        id: 1,
+        author: {
+            avatarUrl: 'https://github.com/fixRuan',
+            name: 'Ruan Pablo',
+            role: 'Web Developer'
+        },
+        content: [
+            {
+                type: 'paragraph',
+                content: 'Fala galeraa 👋'
+            },
+            {
+                type: 'paragraph',
+                content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+            },
+            {
+                type: 'link',
+                content: 'jane.design/doctorcare'
+
+            },
+            {
+                type: 'link',
+                content: '#novoprojeto'
+
+            },
+            {
+                type: 'link',
+                content: '#nlw'
+
+            },
+            {
+                type: 'link',
+                content: '#rocketseat'
+
+            }
+
+        ]
+    }
+]
+
+
 function App() {
     return (
         <>
