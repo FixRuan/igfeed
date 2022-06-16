7d5bc779eba281fbb60aa157510b35797c847f52
1SyuLi <ruan_goio10@hotmail.com>
2022-06-16 15:43:28 -0300

    Post Props

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const posts = [
     {
         id: 1,
         author: {
-            avatarUrl: 'https://github.com/fixRuan',
+            avatarUrl: 'https://github.com/fixRuan.png',
             name: 'Ruan Pablo',
             role: 'Web Developer'
         },
@@ -40,8 +40,47 @@ const posts = [
                 content: '#rocketseat'
 
             }
+        ],
+        publishedAt: new Date('2022-05-03 20:00:00')
+    },
+    {
+        id: 2,
+        author: {
+            avatarUrl: 'https://github.com/filipedeschamps.png',
+            name: 'Filipe Deschamps',
+            role: 'Web Developer'
+        },
+        content: [
+            {
+                type: 'paragraph',
+                content: 'Fala galeraa 👋'
+            },
+            {
+                type: 'paragraph',
+                content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+            },
+            {
+                type: 'link',
+                content: 'jane.design/doctorcare'
 
-        ]
+            },
+            {
+                type: 'link',
+                content: '#novoprojeto'
+
+            },
+            {
+                type: 'link',
+                content: '#nlw'
+
+            },
+            {
+                type: 'link',
+                content: '#rocketseat'
+
+            }
+        ],
+        publishedAt: new Date('2022-05-10 20:00:00')
     }
 ]
 
@@ -56,16 +95,14 @@ function App() {
                 <SideBar />
 
                 <main>
-                    <Post
-                        name="Ruan Pablo"
-                        description='is simply dummy text of the printing and typesetting industry.'
-                    />
-
-                    <Post
-                        name="Ruan Pablo"
-                        description='is simply dummy text of the printing and typesetting industry.'
-                    />
-
+                    {posts.map(post =>
+                        <Post
+                            key={post.id}
+                            author={post.author}
+                            content={post.content}
+                            publishedAt={post.publishedAt}
+                        />
+                    )}
                 </main>
             </div>
         </>
